a5f01a72ae082f0d156e7bfa182931b0f70405fe
Boncoungou_Ulrich_Jean <jeanulrichboncoungou85@gmail.com>
2023-06-29 20:55:03 +0000

    nightcommit

--- actions/questions/postAnswerAction.php
+++ actions/questions/postAnswerAction.php
@@ -1,5 +1,22 @@
 <?php
 
+// require('actions/database.php');
+
+// if(isset($_POST['validate'])){
+
+//     if(!empty($_POST['answer'])){
+
+//         $user_answer = nl2br(htmlspecialchars($_POST['answer']));
+
+//         $insertAnswer = $bdd->prepare('INSERT INTO answers(id_auteur, pseudo_auteur, id_question, contenu)VALUES(?, ?, ?, ?)');
+//         $insertAnswer->execute(array($_SESSION['id'], $_SESSION['pseudo'], $idOfTheQuestion, $user_answer));
+        
+//     }
+// }
+?>
+
+<?php
+
 require('actions/database.php');
 
 if(isset($_POST['validate'])){
@@ -8,8 +25,14 @@ if(isset($_POST['validate'])){
 
         $user_answer = nl2br(htmlspecialchars($_POST['answer']));
 
+        // Obtenir le pseudo de l'auteur à partir de la base de données
+        $getPseudo = $bdd->prepare('SELECT pseudo FROM users WHERE id = ?');
+        $getPseudo->execute(array($_SESSION['id']));
+        $pseudo_auteur = $getPseudo->fetchColumn();
+
+        // Insérer la réponse avec le pseudo correct de l'auteur
         $insertAnswer = $bdd->prepare('INSERT INTO answers(id_auteur, pseudo_auteur, id_question, contenu)VALUES(?, ?, ?, ?)');
-        $insertAnswer->execute(array($_SESSION['id'], $_SESSION['pseudo'], $idOfTheQuestion, $user_answer));
+        $insertAnswer->execute(array($_SESSION['id'], $pseudo_auteur, $idOfTheQuestion, $user_answer));
         
     }
 }
--- actions/questions/showAllQuestionsAction.php
+++ actions/questions/showAllQuestionsAction.php
@@ -1,16 +1,45 @@
 <?php
+// require('actions/database.php');
+
+// //Récupérer les questions par défaut sans recherche
+// $getAllQuestions = $bdd->query('SELECT id, id_auteur, titre, description, pseudo_auteur, date_publication FROM questions ORDER BY id DESC LIMIT 0,5');
+
+// //Vérifier si une recherche a été rentrée par l'utilisateur
+// if(isset($_GET['search']) AND !empty($_GET['search'])){
+
+//     //La recherche
+//     $usersSearch = $_GET['search'];
+
+//     //Récupérer toutes les questions qui correspondent à la recherche (en fonction du titre)
+//     $getAllQuestions = $bdd->query('SELECT id, id_auteur, titre, description, pseudo_auteur, date_publication FROM questions WHERE titre LIKE"%'.$usersSearch.'%" ORDER BY id DESC');
+// }
+?>
+
+<?php
 require('actions/database.php');
 
-//Récupérer les questions par défaut sans recherche
-$getAllQuestions = $bdd->query('SELECT id, id_auteur, titre, description, pseudo_auteur, date_publication FROM questions ORDER BY id DESC LIMIT 0,5');
+// Récupérer les questions avec les informations de l'auteur
+$getAllQuestions = $bdd->query('SELECT q.id, q.id_auteur, q.titre, q.description, u.pseudo AS pseudo_auteur, q.date_publication FROM questions q
+                                JOIN users u ON q.id_auteur = u.id
+                                ORDER BY q.id DESC LIMIT 0,5');
 
-//Vérifier si une recherche a été rentrée par l'utilisateur
-if(isset($_GET['search']) AND !empty($_GET['search'])){
-
-    //La recherche
+// Vérifier si une recherche a été effectuée
+if(isset($_GET['search']) && !empty($_GET['search'])){
     $usersSearch = $_GET['search'];
 
-    //Récupérer toutes les questions qui correspondent à la recherche (en fonction du titre)
-    $getAllQuestions = $bdd->query('SELECT id, id_auteur, titre, description, pseudo_auteur, date_publication FROM questions WHERE titre LIKE"%'.$usersSearch.'%" ORDER BY id DESC');
+    // Récupérer les questions correspondant à la recherche
+    $getAllQuestions = $bdd->query('SELECT q.id, q.id_auteur, q.titre, q.description, u.pseudo AS pseudo_auteur, q.date_publication FROM questions q
+                                    JOIN users u ON q.id_auteur = u.id
+                                    WHERE q.titre LIKE "%'.$usersSearch.'%"
+                                    ORDER BY q.id DESC');
 }
 ?>
+
+<!--  nous utilisons la clause JOIN pour joindre la table "questions" avec la table "users" en fonction de l'id de l'auteur. Nous sélectionnons également le pseudo de l'auteur à partir de la table "users" en utilisant un alias (u.pseudo AS pseudo_auteur). -->
+
+<!-- q.id fait référence à la colonne "id" de la table "questions". Cette colonne est généralement utilisée pour stocker l'identifiant unique de chaque question.
+q.id_auteur fait référence à la colonne "id_auteur" de la table "questions". Cette colonne est utilisée pour stocker l'identifiant de l'auteur qui a posé la question.
+q.titre fait référence à la colonne "titre" de la table "questions". Cette colonne est utilisée pour stocker le titre de chaque question.
+q.description fait référence à la colonne "description" de la table "questions". Cette colonne est utilisée pour stocker une brève description de chaque question.
+q.date_publication fait référence à la colonne "date_publication" de la table "questions". Cette colonne est utilisée pour stocker la date de publication de chaque question.
+Ces expressions sont utilisées dans la requête SQL pour sélectionner spécifiquement ces colonnes de la table "questions" et les récupérer dans le résultat de la requête. Ensuite, ces données sont utilisées pour afficher les informations de chaque question sur votre page d'index. -->
